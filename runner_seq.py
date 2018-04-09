daffc5820242e822143976ca7ab87abdb5f48fa9
jc <you@example.com>
2018-04-09 17:06:44 +0200

    bugfix

--- runner_seq.py
+++ runner_seq.py
@@ -7,9 +7,8 @@ tasks = []
 if len(sys.argv) == 1:
   for order in [5,10,20]:
     for subject in [0,1,2,3,4,5]:
-      graph = 'haxby_subject{}_kalofoliasProjected_4closest'.format(subject)
-      tasks += ['/home/brain/miniconda3/bin/python chebnet_haxby_85.py --subject {} --order {} --graph {}'.format(subject, order, graph)
-                + ' > subject{}_K{}_{}.tmp"'.format(subject, order, graph)]
+      graph = 'haxby_subject{}_kalofoliasprojected_4closest'.format(subject)
+      tasks += ['/home/brain/miniconda3/bin/python chebnet_haxby_85.py --subject {} --order {} --graph {}'.format(subject, order, graph)]
 else:
   tasks = open(sys.argv[1], 'r').readlines()
 
